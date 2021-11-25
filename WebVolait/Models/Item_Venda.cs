3363865ed47da3390fddc871a43d50a8518b75fa
GABRIEL SILVA <gabriel.silva2615@etec.sp.gov.br>
2021-11-25 14:44:06 +0000

    New Classes

---
+++ WebVolait/Models/Item_Venda.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebVolait.Models
+{
+    public class Item_Venda
+    {
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 10, ErrorMessage = "*Código inválido")]
+        public int Cod_ItemVenda { get; set; }
+
+        [Display(Name = "Quantidade")]
+        [Required(ErrorMessage = "*O campo quantidade é obrigatório")]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "*O campo quantidade permite apenas 2 a 10 caracteres")]
+        public string Qtd_ItemVenda { get; set; }
+
+        [Display(Name = "Valor")]
+        [Required(ErrorMessage = "*O campo valor é obrigatório")]
+        [StringLength(10, MinimumLength = 2, ErrorMessage = "*O campo valor permite apenas 2 a 10 caracteres")]
+        public string Valor_ItemVenda { get; set; }
+
+        [Display(Name = "Nota Fiscal")]
+        [Required(ErrorMessage = "*O campo nota fiscal é obrigatório")]
+        [StringLength(15, MinimumLength = 11, ErrorMessage = "*Nota fiscal inválido")]
+        public string NotaFiscal { get; set; }
+
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 10, ErrorMessage = "*Código inválido")]
+        public int Cod_Pacote { get; set; }
+
+
+    }
+}
---
+++ WebVolait/Models/Transporte.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebVolait.Models
+{
+    public class Transporte
+    {
+
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 10, ErrorMessage = "*Código inválido")]
+        public int Cod_Transp { get; set; }
+
+        [Display(Name = "CNPJ")]
+        [Required(ErrorMessage = "*O campo CNPJ é obrigatório")]
+        [StringLength(14, MinimumLength = 9, ErrorMessage = "*CNPJ inválido")]
+        public string CNPJ_Transp { get; set; }
+
+        [Display(Name = "Empresa")]
+        [Required(ErrorMessage = "*O campo empresa é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "*O campo empresa permite apenas 2 a 30 caracteres")]
+        public string Empresa_Transp { get; set; }
+
+        [Display(Name = "Código tipo de transporte")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 10, ErrorMessage = "*Código inválido")]
+        public int Cod_TipoTransp { get; set; }
+
+        // DURACAO FALTANDO
+    }
+}
