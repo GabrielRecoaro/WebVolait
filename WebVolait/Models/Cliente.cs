c84d7892513dae6d43d29ea240906674ba0974ed
GABRIEL SILVA <gabriel.silva2615@etec.sp.gov.br>
2021-11-25 12:05:56 +0000

    Cliente e Passeio Created

---
+++ WebVolait/Models/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebVolait.Models
+{
+    public class Cliente
+    {
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 4, ErrorMessage = "*Código inválido")]
+        public int Cod_Cli { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "*O campo nome é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "*O campo permite apenas 2 a 30 caracteres")]
+        public string Nome_Cli { get; set; }
+
+        [Display(Name = "CPF")]
+        [Required(ErrorMessage = "*O campo CPF é obrigatório")]
+        [StringLength(15, MinimumLength = 11, ErrorMessage = "*CPF inválido")]
+        public string CPF_Cli { get; set; }
+
+        [Display(Name = "E-mail")]
+        [RegularExpression(@"\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*", ErrorMessage = "*E-mail em formato inválido.")]
+        [Required(ErrorMessage = "*O campo E-mail é obrigatório")]
+        public string Email_Cli { get; set; }
+
+        [Display(Name = "Telefone")]
+        [Required(ErrorMessage = "*O campo telefone é obrigatório")]
+        [StringLength(20, MinimumLength = 9, ErrorMessage = "*Número de contato inválido")]
+        public string Telefone_Cli { get; set; }
+
+
+    }
+}
---
+++ WebVolait/Models/Passeio.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebVolait.Models
+{
+    public class Passeio
+    {
+        [Display(Name = "Código")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 10, ErrorMessage = "*Código inválido")]
+        public int Cod_Passeio { get; set; }
+
+        [Display(Name = "Nome")]
+        [Required(ErrorMessage = "*O campo nome é obrigatório")]
+        [StringLength(30, MinimumLength = 2, ErrorMessage = "*O campo nome permite apenas 2 a 30 caracteres")]
+        public string Nome_Passeio { get; set; }
+
+        [Display(Name = "Descrição")]
+        [Required(ErrorMessage = "*O campo descrição é obrigatório")]
+        [StringLength(300, MinimumLength = 10, ErrorMessage = "*O campo permite apenas 10 a 300 caracteres")]
+        public string Desc_Passeio { get; set; }
+
+
+    }
+}
