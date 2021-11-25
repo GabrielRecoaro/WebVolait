eb2eed35c4a1117a25b99ba0406a52e153ba52e1
GABRIEL SILVA <gabriel.silva2615@etec.sp.gov.br>
2021-11-25 16:50:06 +0000

    Finalização Classes

---
+++ WebVolait/Models/Tipo_Pagto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebVolait.Models
+{
+    public class Tipo_Pagto
+    {
+        [Display(Name = "Código do tipo de pagamento")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 4, ErrorMessage = "*Código inválido")]
+        public int Cod_TipoPagto { get; set; }
+
+        /*[Display(Name = "Tipo de hospedagem")]
+        [Required(ErrorMessage = "*O campo tipo de hospedagem é obrigatório")]
+        [StringLength(300, MinimumLength = 10, ErrorMessage = "*O campo permite apenas 10 a 300 caracteres")]
+        public string Tipo_Pagto { get; set; }*/
+
+        //ERRO COM NOME
+    }
+}
---
+++ WebVolait/Models/Venda.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebVolait.Models
+{
+    public class Venda
+    {
+        [Display(Name = "Nota Fiscal")]
+        [Required(ErrorMessage = "*O campo nota fiscal é obrigatório")]
+        [StringLength(15, MinimumLength = 11, ErrorMessage = "*Nota fiscal inválido")]
+        public string NotaFiscal { get; set; }
+
+        [Display(Name = "Data de venda")]
+        [Required(ErrorMessage = "*O campo data de venda é obrigatório")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime JogoDtLanc
+        {
+            get
+            {
+                return this.Data_Venda.HasValue
+                    ? this.Data_Venda.Value
+                    : DateTime.Now;
+            }
+            set
+            {
+                this.Data_Venda = value;
+            }
+
+
+        }
+
+        private DateTime? Data_Venda = null;
+
+        [Display(Name = "Código do funcionário")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 4, ErrorMessage = "*Código inválido")]
+        public int Cod_Func { get; set; }
+
+        [Display(Name = "Código do tipo de pagamento")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 4, ErrorMessage = "*Código inválido")]
+        public int Cod_TipoPagto { get; set; }
+
+        [Display(Name = "Código do cliente")]
+        [Required(ErrorMessage = "*O campo código é obrigatório")]
+        [Range(1, 4, ErrorMessage = "*Código inválido")]
+        public int Cod_Cli { get; set; }
+    }
+}
