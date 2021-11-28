9ee03a61c80437359371a40e5d59e9aabc27b8c8
GABRIEL SILVA <gabriel.silva2615@etec.sp.gov.br>
2021-11-28 12:50:01 +0000

    Conexao e Acoes

---
+++ WebVolait/Repositorio/Acoes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebVolait.Repositorio;
+using MySql.Data.MySqlClient;
+using WebVolait.Models;
+
+namespace WebVolait.Repositorio
+{
+    public class Acoes
+    {
+        Conexao con = new Conexao();
+        MySqlCommand cmd = new MySqlCommand();
+    }
+}
